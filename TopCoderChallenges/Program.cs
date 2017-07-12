8abf2fc6b83fe571795dd55858b1e086632defb7
Freddy juhel <domaine75@gmail.com>
2017-07-12 16:03:47 +0200

    Add problem 1

--- TopCoderChallenges/Program.cs
+++ TopCoderChallenges/Program.cs
@@ -7,6 +7,15 @@ namespace TopCoderChallenges
     private static void Main()
     {
       Action<string> display = Console.WriteLine;
+      display("Top coder - problem 1");
+      /*
+       You are given two s: N and K. Lun the dog is interested in strings that satisfy the following conditions:
+       
+      The string has exactly N characters, each of which is either 'A' or 'B'.
+       The string s has exactly K pairs (i, j) (0 <= i < j <= N-1) such that s[i] = 'A' and s[j] = 'B'.
+
+      If there exists a string that satisfies the conditions, find and return any such string. Otherwise, return an empty string.
+       */
 
       ReadContinue();
     }
@@ -16,7 +25,7 @@ namespace TopCoderChallenges
       Action<string> display = Console.WriteLine;
       display(string.Empty);
       display("Press to exit, any other key to continue...");
-      display(string.Empty); 
+      display(string.Empty);
       return Console.ReadKey(true).Key != ConsoleKey.Escape;
     }
   }
