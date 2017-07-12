3a47616a1c650bb2ff13094d4bca677559424148
JUHEL <freddy.juhel@sogeti.com>
2017-07-12 15:52:16 +0200

    Initial commit

---
+++ TopCoderChallenges/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace TopCoderChallenges
+{
+  internal static class Program
+  {
+    private static void Main()
+    {
+      Action<string> display = Console.WriteLine;
+
+      ReadContinue();
+    }
+
+    private static bool ReadContinue()
+    {
+      Action<string> display = Console.WriteLine;
+      display(string.Empty);
+      display("Press to exit, any other key to continue...");
+      display(string.Empty); 
+      return Console.ReadKey(true).Key != ConsoleKey.Escape;
+    }
+  }
+}
