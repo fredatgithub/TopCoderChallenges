56567305895a130d23641e5892edc2340546f3f0
Freddy juhel <domaine75@gmail.com>
2017-07-12 17:11:29 +0200

    Add problem 2

--- TopCoderChallenges/Program.cs
+++ TopCoderChallenges/Program.cs
@@ -7,7 +7,7 @@ namespace TopCoderChallenges
     private static void Main()
     {
       Action<string> display = Console.WriteLine;
-      display("Top coder - problem 1");
+      display("Top coder - problem's name unknown");
       /*
        You are given two s: N and K. Lun the dog is interested in strings that satisfy the following conditions:
        
@@ -16,6 +16,21 @@ namespace TopCoderChallenges
 
       If there exists a string that satisfies the conditions, find and return any such string. Otherwise, return an empty string.
        */
+
+      display("Top coder - Problem Area");
+      /*
+       Class Name : FarStrings Method Name : find Return Type : String[]Arg Types : (String)
+       Problem Area
+       Problem Statement
+       All strings in this problem are strings of lowercase English letters ('a'-'z').
+
+      Given two strings S and T, their edit distance is defined as the smallest number of steps in which we can change S to T. In each step, we are allowed to make one of the following changes:
+
+      Erasing one arbitrary character of S.
+      Inserting one arbitrary character anywhere into S (including its beginning or end).
+      Replacing one arbitrary character of S by a different character.
+       */
+
 
       ReadContinue();
     }
